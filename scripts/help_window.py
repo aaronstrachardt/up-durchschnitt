c68ad8086b2c909a6036ef57b8fd21017966dff8
aaronstrachardt <strachardt@gmail.com>
2023-05-09 22:32:25 +0200

    idk2

--- scripts/help_window.py
+++ scripts/help_window.py
@@ -6,12 +6,15 @@ class HelpWindow(tk.Toplevel):
         self.title("Hilfe")
         self.geometry("1000x420")
         self.resizable(False, False)
-        self.help_widgets()
 
+<<<<<<< HEAD
     def help_widgets(self):
         self.texts = []
 
         self.text = tk.Label(self, text="Was ist zu tun?", font=("Bahnschrift", 16), bg="#818BAC", fg="#f2f2f2")
+=======
+        self.text = tk.Label(self, text="Was ist zu tun?", font=("Bahnschrift", 16), bg="#818BAC", fg="white")
+>>>>>>> parent of 31d0fbf (Add dark mode)
         self.text.pack(pady=20,anchor="center")
 
         self.text1 = tk.Label(self, text="1. Melde dich bei PULS an", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
@@ -20,12 +23,13 @@ class HelpWindow(tk.Toplevel):
         self.text2 = tk.Label(self, text="2. Gehe zur Leistungsübersicht", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
         self.text2.pack(padx=50, pady=10,anchor="w")
 
-        self.text3 = tk.Label(self, text="3. Speichere die Seite als .html-Datei (Strg+S)", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
+        self.text3 = tk.Label(self, text="3. Speichere die Seite als .html-Datei", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
         self.text3.pack(padx=50, pady=10,anchor="w")
 
-        self.text4 = tk.Label(self, text="4. Wähle im UP-Durchschnittsberechner deine .html-Datei aus", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
+        self.text4 = tk.Label(self, text="4. Wähle deine .html-Datei aus", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
         self.text4.pack(padx=50, pady=10,anchor="w")
 
+<<<<<<< HEAD
         self.text5 = tk.Label(self, text="5. Klicke auf \"Berechnen\" und lass dir deinen aktuellen Durchschnitt ausgeben", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
         self.text5.pack(padx=50, pady=10,anchor="w")
 
@@ -38,6 +42,10 @@ class HelpWindow(tk.Toplevel):
 
 
 
+=======
+        self.text4 = tk.Label(self, text="5. Klicke auf \"Berechnen\" und lass dir deinen aktuellen Durchschnitt ausgeben", font=("Bahnschrift", 12), bg="#f2f2f2", fg="#000000")
+        self.text4.pack(padx=50, pady=10,anchor="w")
+>>>>>>> parent of 31d0fbf (Add dark mode)
 
 
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import ctypes
-import help_window as help_window
+import scripts.help_window as help_window
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
@@ -13,59 +13,32 @@ class MainWindow(tk.Frame):
         self.master.resizable(False, False)
         self.master.geometry("1400x420")
         self.master.configure(bg="#f2f2f2")
-        self.bg_color="white"
         self.create_widgets()
 
     def create_widgets(self):
+<<<<<<< HEAD
         self.buttons=[]
 
         self.text = tk.Label(self.master, text="Willkommen zum (in)offiziellen UP Durchschnittsberechner", font=("Bahnschrift", 16), bg="#f2f2f2", fg="#000000")
+=======
+        self.text = tk.Label(self.master, text="Willkommen  zum (in)offiziellen UP Durchschnittsberechner", font=("Bahnschrift", 16), bg="#f2f2f2", fg="#000000")
+>>>>>>> parent of 31d0fbf (Add dark mode)
         self.text.place(relx=0.5,y=70,anchor="center")
 
         self.file_label = tk.Label(self.master, text="Wähle bitte eine .html-Datei oder .txt-Datei aus", font=("Bahnschrift", 12), bg="#818BAC", fg="#f2f2f2")
         self.file_label.place(relx=0.5,y=140,anchor="center")
 
-        self.choose_file_button = tk.Button(self.master, text="Datei auswählen", command=self.choose_file, bg="#00305E", fg="#f2f2f2", font=("Bahnschrift", 12))
+        self.choose_file_button = tk.Button(self.master, text="Datei auswählen", command=self.choose_file, bg="#00305E", fg="white", font=("Bahnschrift", 12))
         self.choose_file_button.place(relx=0.5,y=210,anchor="center")
-        self.buttons.append(self.choose_file_button)
 
-        self.calculate_button = tk.Button(self.master, text="Berechnen", command=self.calculate, bg="#00305E", fg="#f2f2f2", font=("Bahnschrift", 12))
+        self.calculate_button = tk.Button(self.master, text="Berechnen", command=self.calculate, bg="#00305E", fg="white", font=("Bahnschrift", 12))
         self.calculate_button.place(relx=0.5,y=280,anchor="center")
-        self.buttons.append(self.calculate_button)
 
         self.result_label = tk.Label(self.master, text="", font=("Bahnschrift", 12), bg="#f2f2f2")
         self.result_label.place(relx=0.5,y=350,anchor="center")
 
-        self.menu_button = tk.Button(self.master, text="Help", command=self.open_help_window, bg="#00305E", fg="#f2f2f2", font=("Bahnschrift", 12))
-        self.menu_button.place(x=15,y=15)
-        self.buttons.append(self.menu_button)
-
-        self.dark_mode_button = tk.Button(self.master, text="Dark", command=self.change_mode ,bg="#00305E", fg="#f2f2f2", font=("Bahnschrift", 12))
-        self.dark_mode_button.place(x=15,y=355)
-        self.buttons.append(self.dark_mode_button)
-
-    def change_mode(self):
-        if self.bg_color == "white":
-            self.master.configure(bg="#313338")
-            self.text.configure(fg="#F2F3F5", bg="#313338")
-            self.result_label.configure(fg="#F2F3F5", bg="#313338")
-            self.dark_mode_button.configure(text="Light")
-
-            for button in self.buttons:
-                button.configure(fg="#F2F3F5",bg="#41434A")
-
-            self.bg_color = "black"
-
-        else:
-            self.master.configure(bg="#f2f2f2")
-            self.text.configure(fg="#000000", bg="#f2f2f2")
-            self.result_label.configure(fg="#000000", bg="#f2f2f2")
-            self.dark_mode_button.configure(text="Dark")
-
-            for button in self.buttons:
-                button.configure(fg="#f2f2f2",bg="#00305E")
-
-            self.bg_color = "white"
+        self.menu_button = tk.Button(self.master, text="Help", command=self.open_help_window, bg="#00305E", fg="white", font=("Bahnschrift", 12))
+        self.menu_button.place(x=10,y=10)
 
     def open_help_window(self):
         help_window.HelpWindow()
